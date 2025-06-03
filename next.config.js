86a985cdd5e43ccbcfd0ce4022c0e402383d54d0
TiagoMontes <tiagosantos6303@gmail.com>
2025-06-03 15:32:02 -0500

    fix index.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
+    serverActions: {
+      allowedOrigins: ['localhost:3000', 'bar-opal.vercel.app']
+    }
   },
-  server: {
-    host: '0.0.0.0',
-    port: 3000
-  }
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.json$/,
+      type: 'json',
+    })
+    return config
+  },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
