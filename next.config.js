c0f36c9eb1eab14af6d54fdbcaea544112000c5f
TiagoMontes <tiagosantos6303@gmail.com>
2025-06-03 15:21:22 -0500

    add vercel to use json

--- next.config.js
+++ next.config.js
@@ -6,7 +6,15 @@ const nextConfig = {
   server: {
     host: '0.0.0.0',
     port: 3000
-  }
+  },
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.json$/,
+      type: 'json',
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig 
