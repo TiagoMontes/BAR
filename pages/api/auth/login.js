97c9c83bea505a19c77c2d4b7bbe5494beb319b5
TiagoMontes <tiagosantos6303@gmail.com>
2025-06-03 15:49:39 -0500

    test

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -69,6 +69,6 @@ export default async function handler(req, res) {
     })
   } catch (error) {
     console.error('Login error:', error)
-    res.status(500).json({ message: error })
+    res.status(500).json({ message: error.message })
   }
 } 
