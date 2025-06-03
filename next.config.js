0aa3a660de7df42d558e1c7c9b3e46f2b321f41b
TiagoMontes <tiagosantos6303@gmail.com>
2025-06-03 15:33:31 -0500

    fix vercelignore

--- next.config.js
+++ next.config.js
@@ -1,18 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000', 'bar-opal.vercel.app']
-    }
+    serverActions: true,
   },
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.json$/,
-      type: 'json',
-    })
-    return config
-  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig 
