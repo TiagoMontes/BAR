33e0409a044b26f0e60b9da2c7978b2e84c7bbba
TiagoMontes <tiagosantos6303@gmail.com>
2025-06-22 16:06:37 -0500

    fix button finalizar venda

--- components/VendasInterface.js
+++ components/VendasInterface.js
@@ -684,7 +684,7 @@ export default function VendasInterface({ user }) {
         onClose={() => setCurrentAttendantModal(null)}
       />
 
-      <div className="fixed bottom-0 z-10 left-0 right-0 bg-white p-4 shadow-lg lg:hidden">
+      <div className="fixed bottom-0 z-10 left-0 right-0 bg-white p-4 pb-10 shadow-lg lg:hidden">
         <button
           className="w-full bg-primary text-white py-3 rounded-lg font-semibold hover:bg-primary-dark transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
           disabled={cart.length === 0 || isProcessingSale}
